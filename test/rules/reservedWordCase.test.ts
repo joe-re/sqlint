68994489893de70ccb272ca4e90f3089966c3cbf
joe-re <joe.tialtngo@gmail.com>
2019-08-10 05:49:37 +0900

    add where keyword test

--- test/rules/reservedWordCase.test.ts
+++ test/rules/reservedWordCase.test.ts
@@ -20,3 +20,11 @@ test('from keyword must be uppercase', () => {
   expect(result[0].location.start).toEqual({line: 1, offset: 9,  column: 10 })
   expect(result[0].location.end).toEqual({ line: 1, offset: 13, column: 14 })
 })
+
+test('where keyword must be uppercase', () => {
+  const result = execute('SELECT * FROM foo where id = 1', { rules: { 'reserved-word-case': [ 2, 'upper' ] } })
+  expect(result.length).toEqual(1)
+  expect(result[0].message).toEqual('reserved word must be uppercase')
+  expect(result[0].location.start).toEqual({line: 1, offset: 18,  column: 19 })
+  expect(result[0].location.end).toEqual({ line: 1, offset: 23, column: 24 })
+})
