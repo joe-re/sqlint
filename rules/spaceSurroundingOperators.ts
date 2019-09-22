b36c80f59a8828b62cf1336f7664c6aff6626fd2
joe-re <joe.tialtngo@gmail.com>
2019-09-23 01:46:37 +0800

    add rule for checking surrounding operator space never

--- rules/spaceSurroundingOperators.ts
+++ rules/spaceSurroundingOperators.ts
@@ -18,36 +18,46 @@ export const spaceSurroundingOperators: Rule = {
     if (!['+', '-', '*', '/', '>', '>=', '<', '<=', '!=', '<>', '='].includes(context.node.operator)) {
       return
     }
-    const regexp = new RegExp(` ${context.node.operator} $`)
-    const part = context.getSQL(context.node.left.location, { after: context.node.operator.length + 2})
-    const result = regexp.exec(part)
-    if (!result) {
-      const start = {
-        line: context.node.left.location.end.line,
-        offset: context.node.left.location.end.offset,
-        column: context.node.left.location.end.column
+    if (context.config.options[0] === 'always') {
+      const regexp = new RegExp(` ${context.node.operator} $`)
+      const part = context.getSQL(context.node.left.location, { after: context.node.operator.length + 2})
+      const result = regexp.exec(part)
+      if (!result) {
+        const start = {
+          line: context.node.left.location.end.line,
+          offset: context.node.left.location.end.offset,
+          column: context.node.left.location.end.column
+        }
+        const end = {
+          line: context.node.left.location.end.line,
+          offset: context.node.left.location.end.offset + 2 + context.node.operator.length,
+          column: context.node.left.location.end.column + 2 + context.node.operator.length
+        }
+        return {
+          message: META.messages.always,
+          location: { start, end }
+        }
       }
-      const end = {
-        line: context.node.left.location.end.line,
-        offset: context.node.left.location.end.offset + 2 + context.node.operator.length,
-        column: context.node.left.location.end.column + 2 + context.node.operator.length
-      }
-      return {
-        message: META.messages.always,
-        location: { start, end }
+    } else if (context.config.options[0] === 'never') {
+      const regexp = new RegExp(`[^\s]${context.node.operator}[^\s]$`)
+      const part = context.getSQL(context.node.left.location, { after: context.node.operator.length + 1})
+      const result = regexp.exec(part)
+      if (!result) {
+        const start = {
+          line: context.node.left.location.end.line,
+          offset: context.node.left.location.end.offset,
+          column: context.node.left.location.end.column
+        }
+        const end = {
+          line: context.node.left.location.end.line,
+          offset: context.node.left.location.end.offset + 1 + context.node.operator.length,
+          column: context.node.left.location.end.column + 1 + context.node.operator.length
+        }
+        return {
+          message: META.messages.never,
+          location: { start, end }
+        }
       }
     }
-    // if (context.config.options[0] === 'always' && /[a-z]/.test(context.node.operator)) {
-    //   return {
-    //     message: META.messages.always,
-    //     location: node.location
-    //   }
-    // }
-    // if (config.options[0] === 'never' && /[A-Z]/.test(node.value)) {
-    //   return {
-    //     message: META.messages.never,
-    //     location: node.location
-    //   }
-    // }
   }
 }
--- test/rules/spaceSurroundingOperators.test.ts
+++ test/rules/spaceSurroundingOperators.test.ts
@@ -20,3 +20,24 @@ WHERE foo.a > 1
   expect(result[1].location.start).toEqual({line: 6, offset: 61,  column: 12 })
   expect(result[1].location.end).toEqual({ line: 6, offset: 64, column: 15 })
 })
+
+test('space surrounding never', () => {
+  const sql = `
+SELECT *
+FROM foo
+WHERE foo.a > 1
+   OR foo.b>=2
+  AND foo.c=true
+   OR foo.d <> false
+`
+  const result = execute(sql, { rules: { 'space-surrounding-operators': [ 2, 'never' ] } })
+  expect(result.length).toEqual(2)
+
+  expect(result[0].message).toEqual('space surrounding never')
+  expect(result[0].location.start).toEqual({line: 4, offset: 30,  column: 12 })
+  expect(result[0].location.end).toEqual({ line: 4, offset: 32, column: 14 })
+
+  expect(result[1].message).toEqual('space surrounding never')
+  expect(result[1].location.start).toEqual({line: 7, offset: 78,  column: 12 })
+  expect(result[1].location.end).toEqual({ line: 7, offset: 81, column: 15 })
+})
